859df12736b6046bd709705b57f869e6f1d2b0f5
SANCHO Ugo <Ugo.Sancho@bordeaux-inp.fr>
2025-11-21 12:16:18 +0100

    Homographie presque fini

--- fonctions.py
+++ fonctions.py
@@ -2,7 +2,35 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Nov 21 11:17:27 2025
-
 @author: dridri & Gosan
 """
 
+import numpy as np
+
+def homography_estimate(x1, y1, x2, y2):
+    assert(len(x1) == len(y1) == len(x2) == len(y2))
+    
+    A = np.zeros((8, len(x1)*2))
+    B = np.zeros((1, len(x1)*2))
+    
+    for i in range (len(x1)):
+        
+        A[2*i] = [x1[i], y1[i], 1, 0, 0, 0, -x1[i]*x2[i], -y1[i]*y2[i]]
+        A[2*i+1] = [0, 0, 0, x1[i], y1[i], 1, -x1[i]*x2[i], -y1[i]*y2[i]]
+        
+        B[2*i] = x2[i]
+        B[2*i+1] = y2[i]
+    
+    return H
+
+def homography_apply(H, x1, y1):
+    assert(len(x1) == len(y1))
+    
+    x2 = []
+    y2 = []
+    
+    for i in range (len(x1)):
+        x2.append((H[0,0]*x1[i] + H[0,1]*y1[i] + H[0,2])/ (H[2,0]*x1[i] + H[2,1]*y1[i] + H[2,2]))
+        y2.append((H[1,0]*x1[i] + H[1,1]*y1[i] + H[1,2])/ (H[2,0]*x1[i] + H[2,1]*y1[i] + H[2,2]))
+    
+    return (x2, y2)
