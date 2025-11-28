3f00ca5c09e3ad4d55d4372bc2be204f2d7631a6
SANCHO Ugo <Ugo.Sancho@bordeaux-inp.fr>
2025-11-28 10:19:21 +0100

    Fonctions Homography validée

--- fonctions.py
+++ fonctions.py
@@ -10,16 +10,23 @@ import numpy as np
 def homography_estimate(x1, y1, x2, y2):
     assert(len(x1) == len(y1) == len(x2) == len(y2))
     
-    A = np.zeros((8, len(x1)*2))
-    B = np.zeros((1, len(x1)*2))
+    N = len(x1)
+    assert(N >= 4)
+    A = np.zeros((2*N, 8))
+    B = np.zeros(2*N)
     
-    for i in range (len(x1)):
+    for i in range (N):
         
-        A[2*i] = [x1[i], y1[i], 1, 0, 0, 0, -x1[i]*x2[i], -y1[i]*y2[i]]
-        A[2*i+1] = [0, 0, 0, x1[i], y1[i], 1, -x1[i]*x2[i], -y1[i]*y2[i]]
+        A[2*i] = [x1[i], y1[i], 1, 0, 0, 0, -x1[i]*x2[i], -y1[i]*x2[i]]
+        A[2*i+1] = [0, 0, 0, x1[i], y1[i], 1, -x1[i]*y2[i], -y1[i]*y2[i]]
         
         B[2*i] = x2[i]
         B[2*i+1] = y2[i]
+        
+    X = np.linalg.lstsq(A, B, rcond=None)[0]
+    X = np.append(X, 1)
+    
+    H = np.reshape(X, (3,3))
     
     return H
 
@@ -34,3 +41,7 @@ def homography_apply(H, x1, y1):
         y2.append((H[1,0]*x1[i] + H[1,1]*y1[i] + H[1,2])/ (H[2,0]*x1[i] + H[2,1]*y1[i] + H[2,2]))
     
     return (x2, y2)
+
+
+
+
